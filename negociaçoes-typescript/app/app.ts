f1d761d6e14c8bb0e6cdd01ca450b3bfa5917187
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-08-11 22:13:57 -0300

    evolução até a aula 4

--- negociaçoes-typescript/app/app.ts
+++ negociaçoes-typescript/app/app.ts
@@ -1,5 +1,10 @@
-import { Negociacao } from "./models/negociacao.js";
+import { negociacaoController } from "./controllers/negociacao-controller.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.volume);
+const negociacao = new negociacaoController()
+const form = document.querySelector('.form')
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    negociacao.adiciona()
+})
 
--- negociaçoes-typescript/dist/js/app.js
+++ negociaçoes-typescript/dist/js/app.js
@@ -1,3 +1,7 @@
-import { Negociacao } from "./models/negociacao.js";
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.volume);
+import { negociacaoController } from "./controllers/negociacao-controller.js";
+const negociacao = new negociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    negociacao.adiciona();
+});
