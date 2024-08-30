52bfc9a21cd5210a6c8a4e1612bafa95feba12e4
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-08-29 21:49:44 -0300

    condigurando meu boardTypeScript com minhas preferencias

--- board-vite-typescript/src/main.ts
+++ board-vite-typescript/src/main.ts
@@ -1,4 +1,21 @@
+import { TarefasController } from "./controllers/tarefas-controller.js";
 
+const form = document.querySelector("#form");
+const tarefasController = new TarefasController();
 
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    tarefasController.adiciona();
+  });
+}
 
+document.addEventListener("click", (e) => {
+  const targetButton = e.target as HTMLElement;
 
+  if (targetButton && targetButton.classList.contains("btn__remove")) {
+    const tarefaIdDelete = Number(targetButton.id);
+    tarefasController.deleta(tarefaIdDelete);
+    return;
+  }
+});
