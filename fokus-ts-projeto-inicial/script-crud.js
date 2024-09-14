e1ee87e59f3a970dbb978e21b23ea097ad2c4d2d
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-09-14 19:42:25 -0300

    finalizei o curso de typeScript com paradigma funcional.

--- fokus-ts-projeto-inicial/script-crud.js
+++ fokus-ts-projeto-inicial/script-crud.js
@@ -1,25 +1,136 @@
 let estadoInicial = {
     tarefas: [
         {
+            id: "asjkasdj",
             descricao: "Tarefa concluída",
             concluida: true,
         },
         {
+            id: "adhndmsw",
             descricao: "Tarefa pendente 1",
             concluida: false,
         },
         {
+            id: "asbgrtyu",
             descricao: "Tarefa pendente 2",
             concluida: false,
         },
     ],
     tarefaSelecionada: null,
+    tarefaAhEditar: null,
 };
+const gerarId = () => Date.now().toString(36);
+const jahExiste = (tarefa) => estadoInicial.tarefas.find(tarefaExistente => tarefaExistente.descricao === tarefa.descricao);
 const selecionarTarefa = (estado, tarefa) => {
     return Object.assign(Object.assign({}, estado), { tarefaSelecionada: tarefa === estado.tarefaSelecionada ? null : tarefa });
 };
+const editarTarefa = (estado, tarefa) => {
+    return Object.assign(Object.assign({}, estado), { tarefaAhEditar: tarefa });
+};
+const concluirTarefa = (estado, tarefa) => {
+    return Object.assign(Object.assign({}, estado), { tarefas: estado.tarefas.map((tarefaMapeada) => {
+            if (tarefaMapeada === tarefa) {
+                return Object.assign(Object.assign({}, tarefa), { concluida: !tarefa.concluida });
+            }
+            return tarefaMapeada;
+        }) });
+};
+const adicionarTarefa = (estado, tarefa) => {
+    if (jahExiste(tarefa)) {
+        throw new Error(`Tarefa já adicionada, use uma variação na sua descrição de tarefa. 
+      Ex: ${tarefa.descricao} Variação de informação!`);
+    }
+    if (estado.tarefaAhEditar) {
+        return Object.assign(Object.assign({}, estado), { tarefas: estado.tarefas.map(tarefaExistente => {
+                var _a;
+                if (tarefaExistente.id === ((_a = estado.tarefaAhEditar) === null || _a === void 0 ? void 0 : _a.id)) {
+                    return Object.assign(Object.assign({}, tarefaExistente), { descricao: tarefa.descricao });
+                }
+                ;
+                return tarefaExistente;
+            }), tarefaAhEditar: null });
+    }
+    return Object.assign(Object.assign({}, estado), { tarefas: [...estado.tarefas, tarefa] });
+};
+const cancelarTarefa = (form) => {
+    form.classList.toggle("hidden");
+    form.reset();
+};
+const deletarTarefa = (estado, textArea) => {
+    const tarefaAhDeletar = textArea.value;
+    return Object.assign(Object.assign({}, estado), { tarefas: estado.tarefas.filter((tarefa) => tarefa.descricao !== tarefaAhDeletar) });
+};
+const removerTarefasConcluidas = (estado) => {
+    var _a;
+    if ((_a = estado.tarefaSelecionada) === null || _a === void 0 ? void 0 : _a.concluida) {
+        estado.tarefaSelecionada = null;
+    }
+    return Object.assign(Object.assign({}, estado), { tarefas: estado.tarefas.filter((tarefa) => !tarefa.concluida) });
+};
+const removerTodasTarefas = (estado) => {
+    estado.tarefaSelecionada = null;
+    atualizarUI();
+    return Object.assign(Object.assign({}, estado), { tarefas: [] });
+};
 const atualizarUI = () => {
     const ulTarefas = document.querySelector(".app__section-task-list");
+    const formAdicionarTarefa = document.querySelector(".app__form-add-task");
+    const labelForm = document.querySelector(".app__form-label");
+    const emAndamentoArea = document.querySelector(".app__section-active-task-label");
+    const btnAdicionarTarefas = document.querySelector(".app__button--add-task");
+    const textArea = document.querySelector(".app__form-textarea");
+    const btnCancelarTarefa = document.querySelector(".app__form-footer__button--cancel");
+    const btnRemoverUmaTarefa = document.querySelector(".app__form-footer__button--delete");
+    const btnRemoverTarefas = document.querySelector("#btn-remover-todas");
+    const btnRemoverConcluidas = document.querySelector("#btn-remover-concluidas");
+    if (!btnRemoverConcluidas) {
+        throw new Error("btnRemoverConcluidas não foi encontrado.");
+    }
+    btnRemoverConcluidas.onclick = () => {
+        estadoInicial = removerTarefasConcluidas(estadoInicial);
+        atualizarUI();
+    };
+    btnRemoverTarefas.onclick = () => {
+        estadoInicial = removerTodasTarefas(estadoInicial);
+        atualizarUI();
+    };
+    if (!btnAdicionarTarefas) {
+        throw Error("btnAdicionarTarefas não foi encontrado.");
+    }
+    btnAdicionarTarefas.onclick = () => {
+        labelForm.innerHTML = "Adicionando Tarefa";
+        formAdicionarTarefa === null || formAdicionarTarefa === void 0 ? void 0 : formAdicionarTarefa.classList.toggle("hidden");
+    };
+    formAdicionarTarefa.onsubmit = (event) => {
+        event.preventDefault();
+        const descricao = textArea.value;
+        if (estadoInicial.tarefaAhEditar) {
+            estadoInicial = adicionarTarefa(estadoInicial, Object.assign(Object.assign({}, estadoInicial.tarefaAhEditar), { descricao: descricao }));
+        }
+        else {
+            estadoInicial = adicionarTarefa(estadoInicial, {
+                id: gerarId(),
+                descricao: descricao,
+                concluida: false,
+            });
+        }
+        formAdicionarTarefa === null || formAdicionarTarefa === void 0 ? void 0 : formAdicionarTarefa.classList.add("hidden");
+        atualizarUI();
+    };
+    if (!btnCancelarTarefa) {
+        throw new Error("btnCancelarTarefa não foi encontrado!");
+    }
+    btnCancelarTarefa.onclick = () => {
+        cancelarTarefa(formAdicionarTarefa);
+    };
+    if (!btnRemoverUmaTarefa) {
+        throw new Error("btnRemoverUmaTarefa não foi encontrado!");
+    }
+    btnRemoverUmaTarefa.onclick = () => {
+        estadoInicial = deletarTarefa(estadoInicial, textArea);
+        labelForm.innerHTML = 'Adicionando Tarefa';
+        atualizarUI();
+    };
     if (ulTarefas)
         ulTarefas.innerHTML = "";
     estadoInicial.tarefas.forEach((tarefa) => {
@@ -39,18 +150,50 @@ const atualizarUI = () => {
         const paragraph = document.createElement("p");
         paragraph.classList.add("app__section-task-list-item-description");
         paragraph.textContent = tarefa.descricao;
-        const button = document.createElement('button');
-        button.classList.add('app_button-edit');
-        const editIcon = document.createElement('img');
-        editIcon.setAttribute('src', '/imagens/edit.png');
+        const button = document.createElement("button");
+        button.classList.add("app_button-edit");
+        const editIcon = document.createElement("img");
+        editIcon.setAttribute("src", "/imagens/edit.png");
         button.appendChild(editIcon);
+        button.addEventListener("click", (e) => {
+            e.stopPropagation();
+            estadoInicial = editarTarefa(estadoInicial, tarefa);
+            console.log(estadoInicial);
+            if (estadoInicial.tarefaAhEditar) {
+                labelForm.innerHTML = "Editando Tarefa";
+                formAdicionarTarefa === null || formAdicionarTarefa === void 0 ? void 0 : formAdicionarTarefa.classList.remove("hidden");
+                textArea.value = estadoInicial.tarefaAhEditar.descricao;
+            }
+        });
         if (tarefa.concluida) {
-            button.setAttribute('disabled', 'true');
-            li.classList.add('app__section-task-list-item-complete');
+            button.setAttribute("disabled", "true");
+            li.classList.add("app__section-task-list-item-complete");
         }
         li.appendChild(svgIcon);
         li.appendChild(paragraph);
         li.appendChild(button);
+        li.addEventListener("click", () => {
+            estadoInicial = selecionarTarefa(estadoInicial, tarefa);
+            atualizarUI();
+        });
+        if (estadoInicial.tarefaSelecionada === tarefa) {
+            li.classList.add("app__section-task-list-item-active");
+            emAndamentoArea.innerText = `#Em andamento: ${tarefa.descricao}`;
+        }
+        if (estadoInicial.tarefaSelecionada === null) {
+            emAndamentoArea.innerText = `#Em andamento:`;
+        }
+        svgIcon.onclick = () => {
+            estadoInicial = concluirTarefa(estadoInicial, tarefa);
+            atualizarUI();
+        };
         ulTarefas === null || ulTarefas === void 0 ? void 0 : ulTarefas.appendChild(li);
     });
 };
+document.addEventListener('TarefaFinalizada', () => {
+    if (estadoInicial.tarefaSelecionada) {
+        estadoInicial.tarefaSelecionada.concluida = true;
+        atualizarUI();
+    }
+});
+atualizarUI();
--- fokus-ts-projeto-inicial/script-crud.ts
+++ fokus-ts-projeto-inicial/script-crud.ts
@@ -1,4 +1,5 @@
 interface Tarefa {
+  id: string;
   descricao: string;
   concluida: boolean;
 }
@@ -6,6 +7,7 @@ interface Tarefa {
 interface EstadoAplicacao {
   tarefas: Tarefa[];
   tarefaSelecionada: Tarefa | null;
+  tarefaAhEditar: Tarefa | null;
 }
 
 //Um dos conceitos de paradigma funcional: na maioria dos casos, Funções devem ser puras e não devem gerar efeitos colaterais.
@@ -13,30 +15,219 @@ interface EstadoAplicacao {
 let estadoInicial: EstadoAplicacao = {
   tarefas: [
     {
+      id: "asjkasdj",
       descricao: "Tarefa concluída",
       concluida: true,
     },
     {
+      id: "adhndmsw",
       descricao: "Tarefa pendente 1",
       concluida: false,
     },
     {
+      id: "asbgrtyu",
       descricao: "Tarefa pendente 2",
       concluida: false,
     },
   ],
   tarefaSelecionada: null,
+  tarefaAhEditar: null,
 };
 
-const selecionarTarefa = (estado: EstadoAplicacao, tarefa: Tarefa) => {
+const gerarId = (): string => Date.now().toString(36);
+
+const jahExiste = (tarefa: Tarefa) => estadoInicial.tarefas.find(tarefaExistente => tarefaExistente.descricao === tarefa.descricao)
+
+const selecionarTarefa = (
+  estado: EstadoAplicacao,
+  tarefa: Tarefa
+): EstadoAplicacao => {
   return {
     ...estado,
     tarefaSelecionada: tarefa === estado.tarefaSelecionada ? null : tarefa,
   };
 };
 
+const editarTarefa = (
+  estado: EstadoAplicacao,
+  tarefa: Tarefa
+): EstadoAplicacao => {
+  return {
+    ...estado,
+    tarefaAhEditar: tarefa,
+  };
+};
+
+const concluirTarefa = (
+  estado: EstadoAplicacao,
+  tarefa: Tarefa
+): EstadoAplicacao => {
+  return {
+    ...estado,
+    tarefas: estado.tarefas.map((tarefaMapeada) => {
+      if (tarefaMapeada === tarefa) {
+        return { ...tarefa, concluida: !tarefa.concluida };
+      }
+
+      return tarefaMapeada;
+    }),
+  };
+};
+
+const adicionarTarefa = (estado: EstadoAplicacao, tarefa: Tarefa): EstadoAplicacao => {
+
+  if(jahExiste(tarefa)) {
+    throw new Error(`Tarefa já adicionada, use uma variação na sua descrição de tarefa. 
+      Ex: ${tarefa.descricao} Variação de informação!`
+    )
+  } 
+
+  if (estado.tarefaAhEditar) {
+    return {
+      ...estado,
+      tarefas: estado.tarefas.map(tarefaExistente => {
+        if(tarefaExistente.id === estado.tarefaAhEditar?.id) {
+          return {...tarefaExistente, descricao: tarefa.descricao}
+        };
+        return tarefaExistente;
+      }),
+
+      tarefaAhEditar: null
+    }
+  }
+  return {
+    ...estado,
+    tarefas: [...estado.tarefas, tarefa],
+  };
+};
+
+const cancelarTarefa = (form: HTMLFormElement) => {
+  form.classList.toggle("hidden");
+  form.reset();
+};
+
+const deletarTarefa = (
+  estado: EstadoAplicacao,
+  textArea: HTMLTextAreaElement
+): EstadoAplicacao => {
+  const tarefaAhDeletar = textArea.value;
+  return {
+    ...estado,
+    tarefas: estado.tarefas.filter(
+      (tarefa) => tarefa.descricao !== tarefaAhDeletar
+    ),
+  };
+};
+
+const removerTarefasConcluidas = (estado: EstadoAplicacao): EstadoAplicacao => {
+  if (estado.tarefaSelecionada?.concluida) {
+    estado.tarefaSelecionada = null;
+  }
+  return {
+    ...estado,
+    tarefas: estado.tarefas.filter((tarefa) => !tarefa.concluida),
+  };
+};
+
+const removerTodasTarefas = (estado: EstadoAplicacao): EstadoAplicacao => {
+  estado.tarefaSelecionada = null;
+  atualizarUI();
+  return {
+    ...estado,
+    tarefas: [],
+  };
+};
+
 const atualizarUI = () => {
   const ulTarefas = document.querySelector(".app__section-task-list");
+  const formAdicionarTarefa = document.querySelector<HTMLFormElement>(
+    ".app__form-add-task"
+  );
+  const labelForm =
+    document.querySelector<HTMLLabelElement>(".app__form-label");
+  const emAndamentoArea = document.querySelector<HTMLParagraphElement>(
+    ".app__section-active-task-label"
+  );
+  const btnAdicionarTarefas = document.querySelector<HTMLButtonElement>(
+    ".app__button--add-task"
+  );
+  const textArea = document.querySelector<HTMLTextAreaElement>(
+    ".app__form-textarea"
+  );
+  const btnCancelarTarefa = document.querySelector<HTMLButtonElement>(
+    ".app__form-footer__button--cancel"
+  );
+  const btnRemoverUmaTarefa = document.querySelector<HTMLButtonElement>(
+    ".app__form-footer__button--delete"
+  );
+  const btnRemoverTarefas =
+    document.querySelector<HTMLButtonElement>("#btn-remover-todas");
+  const btnRemoverConcluidas = document.querySelector<HTMLButtonElement>(
+    "#btn-remover-concluidas"
+  );
+
+  if (!btnRemoverConcluidas) {
+    throw new Error("btnRemoverConcluidas não foi encontrado.");
+  }
+
+  btnRemoverConcluidas.onclick = () => {
+    estadoInicial = removerTarefasConcluidas(estadoInicial);
+    atualizarUI();
+  };
+
+  btnRemoverTarefas!.onclick = () => {
+    estadoInicial = removerTodasTarefas(estadoInicial);
+    atualizarUI();
+  };
+
+  if (!btnAdicionarTarefas) {
+    throw Error("btnAdicionarTarefas não foi encontrado.");
+  }
+
+  btnAdicionarTarefas.onclick = () => {
+    labelForm!.innerHTML = "Adicionando Tarefa";
+    formAdicionarTarefa?.classList.toggle("hidden");
+  };
+
+  formAdicionarTarefa!.onsubmit = (event) => {
+    event.preventDefault();
+    const descricao = textArea!.value;
+
+    if(estadoInicial.tarefaAhEditar) {
+      estadoInicial = adicionarTarefa(estadoInicial, {
+        ...estadoInicial.tarefaAhEditar,
+        descricao: descricao
+      })
+    } else {
+      estadoInicial = adicionarTarefa(estadoInicial, {
+      id: gerarId(),
+      descricao: descricao,
+      concluida: false,
+    });
+    }
+
+    formAdicionarTarefa?.classList.add("hidden");
+    atualizarUI();
+  };
+
+  if (!btnCancelarTarefa) {
+    throw new Error("btnCancelarTarefa não foi encontrado!");
+  }
+
+  btnCancelarTarefa.onclick = () => {
+    cancelarTarefa(formAdicionarTarefa!);
+  };
+
+  if (!btnRemoverUmaTarefa) {
+    throw new Error("btnRemoverUmaTarefa não foi encontrado!");
+  }
+
+  btnRemoverUmaTarefa.onclick = () => {
+    estadoInicial = deletarTarefa(estadoInicial, textArea!);
+    labelForm!.innerHTML = 'Adicionando Tarefa'
+    atualizarUI();
+  };
+
   if (ulTarefas) ulTarefas.innerHTML = "";
 
   estadoInicial.tarefas.forEach((tarefa) => {
@@ -61,24 +252,60 @@ const atualizarUI = () => {
     paragraph.classList.add("app__section-task-list-item-description");
     paragraph.textContent = tarefa.descricao;
 
-    const button = document.createElement('button')
-    button.classList.add('app_button-edit')
+    const button = document.createElement("button");
+    button.classList.add("app_button-edit");
+    const editIcon = document.createElement("img");
+    editIcon.setAttribute("src", "/imagens/edit.png");
+    button.appendChild(editIcon);
 
-    const editIcon = document.createElement('img')
-    editIcon.setAttribute('src', '/imagens/edit.png')
+    button.addEventListener("click", (e) => {
+      e.stopPropagation();
+      estadoInicial = editarTarefa(estadoInicial, tarefa);
+      console.log(estadoInicial);
 
-    button.appendChild(editIcon)
+      if (estadoInicial.tarefaAhEditar) {
+        labelForm!.innerHTML = "Editando Tarefa";
+        formAdicionarTarefa?.classList.remove("hidden");
+        textArea!.value = estadoInicial.tarefaAhEditar.descricao;
+      }
+    });
 
     if (tarefa.concluida) {
-        button.setAttribute('disabled', 'true')
-        li.classList.add('app__section-task-list-item-complete')
+      button.setAttribute("disabled", "true");
+      li.classList.add("app__section-task-list-item-complete");
     }
-  
 
-    li.appendChild(svgIcon)
-    li.appendChild(paragraph)
-    li.appendChild(button)
+    li.appendChild(svgIcon);
+    li.appendChild(paragraph);
+    li.appendChild(button);
+
+    li.addEventListener("click", () => {
+      estadoInicial = selecionarTarefa(estadoInicial, tarefa);
+      atualizarUI();
+    });
+
+    if (estadoInicial.tarefaSelecionada === tarefa) {
+      li.classList.add("app__section-task-list-item-active");
+      emAndamentoArea!.innerText = `#Em andamento: ${tarefa.descricao}`;
+    }
+
+    if (estadoInicial.tarefaSelecionada === null) {
+      emAndamentoArea!.innerText = `#Em andamento:`;
+    }
+
+    svgIcon.onclick = () => {
+      estadoInicial = concluirTarefa(estadoInicial, tarefa);
+      atualizarUI();
+    };
 
     ulTarefas?.appendChild(li);
   });
 };
+
+document.addEventListener('TarefaFinalizada', () => {
+  if(estadoInicial.tarefaSelecionada) {
+    estadoInicial.tarefaSelecionada.concluida = true
+    atualizarUI()
+  }
+})
+atualizarUI();
